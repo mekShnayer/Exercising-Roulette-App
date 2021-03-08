3126cb4ba3f4d70bdaf8f05a26eb8d9b557a9133
shani <shani.mek@gmail.com>
2021-03-08 14:42:01 +0200

    adding logo image component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import ExerciseWheel from './components/ExerciseWheel';
 import './App.css';
 // import RouletteWheel from './components/RouletteWheel';
-// import Logo from './components/logo';
-// import 'logo.jpg';
+import Logo from './components/logo';
+import logoPic from './components/logo.png';
 
 function App() {
   return (
     <div className="App">
-      {/* <Logo src='' /> */}
-      <div> logo component: will power</div>
+      <Logo src={logoPic} />
+      
       <div> filter-box component: tabata / reps / random</div>
       <div> open personal zone </div>
       <ExerciseWheel />
--- src/components/logo.js
+++ src/components/logo.js
@@ -4,7 +4,7 @@ class Logo extends React.Component {
     render() {
         return (
             <div>
-                <img src={this.props.src} />
+                <img src={this.props.src} width='100px' />
             </div>
         )
     }
