8a80c07cda04f483347664767a9ccabf430c6ad6
shani <shani.mek@gmail.com>
2021-03-14 18:24:45 +0200

    creating a dialog component

---
+++ src/components/Dialog.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Dialog extends React.Component {
+    render() {
+        return (
+            <div>
+                I'm a dialog
+            </div>
+        )
+    }
+}
+
+export default Dialog;
