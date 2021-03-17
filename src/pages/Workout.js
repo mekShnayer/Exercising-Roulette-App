450bb5a6c67e9b1d54f64e33795c7bed69ec0520
shani <shani.mek@gmail.com>
2021-03-17 12:23:57 +0200

    editing workout page

--- src/pages/Workout.js
+++ src/pages/Workout.js
@@ -1,7 +1,39 @@
 import React from 'react';
+const rest = { name: 'rest', visual: 'rest photo', time: 10 }
+let legworkout = [
+    { name: '1', visual: '', time: 20 },
+    rest,
+    { name: '2', visual: '', time: 20 },
+    rest,
+    { name: '3', visual: '', time: 20 },
+    rest,
+    { name: '4', visual: '', time: 20 },
+    rest,
+    { name: '5', visual: '', time: 20 },
+    rest,
+    { name: '6', visual: '', time: 20 },
+    rest,
+    { name: '7', visual: '', time: 20 },
+    rest,
+    { name: '8', visual: '', time: 20 },
+    rest,
 
-function Workout() {
-    return (<div>workout page</div>)
+]
+
+
+
+function Workout(workout,legworkout) {
+    let work = workout.map(exercise => {
+        const work = (
+            <div>
+                <h1>{exercise.name}</h1>
+                <img src={exercise.visual} />
+                <h2>timer: {exercise.time}</h2>
+            </div>
+        )
+        setTimeout(work, exercise.time*1000)
+    })
+    return ({work})
 }
 
 export default Workout;
