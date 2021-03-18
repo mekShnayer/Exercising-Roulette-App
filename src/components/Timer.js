1738fd2dcbcbc9d3dab260287ef35a5abfeab7bc
shani <shani.mek@gmail.com>
2021-03-18 18:09:53 +0200

    workout page edit

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -6,37 +6,35 @@ class Timer extends React.Component {
         this.state = {
             time: 0,
             endTime: this.props.time,
-            
+
         }
     }
     updateTime = () => {
         let { time, endTime } = this.state;
-        if (time < endTime+1) {
+        if (time < endTime + 1) {
             this.setState({ time: this.state.time + 1 })
-            console.log('added!',time)
-        }else{clearInterval(this.timer)}
+        } else { clearInterval(this.timer) }
     }
     timer = () => {
         setInterval(() => {
             this.updateTime()
         }, 1000);
     }
-    componentDidMount(){
+    componentDidMount() {
         this.timer();
     }
     render() {
         let { time, endTime } = this.state;
-        
-
-        return (time < endTime+1 ? (
-            <div>
+        console.log(time, endTime)
+        return (time < endTime + 1 ? (
+            <div style={{backgroundColor: 'pink'}}>
                 time: {time} , endtime: {endTime}
             </div>
-        ) : (<div>end of time!</div>
+        ) : (<div style={{backgroundColor: 'pink'}}>end of time!</div>
         )
         )
     }
-    
+
     componentWillUnmount() {
         clearInterval(this.timer);
     }
--- src/pages/Workout.js
+++ src/pages/Workout.js
@@ -5,7 +5,7 @@ const rest = { name: 'rest', visual: 'rest photo', time: 10 };
 let legworkout = [
     { name: '1', visual: '', time: 20 },
     rest,
-    { name: '2', visual: '', time: 20 },
+    { name: '2', visual: '', time: 30 },
     rest,
     { name: '3', visual: '', time: 20 },
     rest,
@@ -21,28 +21,60 @@ let legworkout = [
     rest,
 ]
 class Workout extends React.Component {
-    render() {
-        let exercises = legworkout.map((exercise, index) => {
-            let exerciseDiv = (
-                <div key={index}>
-                    <h1>exercise name : {exercise.name}</h1>
-                    <div> exercise visual : {exercise.visual}</div>
-                    <h2>exercise time : {exercise.time}</h2>
-                </div>
-            )
-            return (
-                <div>{exerciseDiv}</div>
-            )
-        });
+    constructor() {
+        super();
+        this.state = {
+            exerciseArr: legworkout,
+            index: 0,
+            
+        }
+    }
+    updateIndex = () => {
+        let { index, exerciseArr } = this.state;
+        let time = exerciseArr[index].time;
+        if (index <= exerciseArr.length - 2) {
+            this.setState({ index: index + 1 })
+            console.log('set state success')
+        } else { clearInterval(this.updateInterval) }
+    }
+    updateInterval = () => {
+        let { index, exerciseArr } = this.state;
+        let time = exerciseArr[index].time;
+        setInterval(() => {
+            this.updateIndex()
+        }, time * 1000);
+    }
+    componentDidMount() {
+        // let { index, exerciseArr } = this.state;
 
+        this.updateInterval();
+
+    }
+
+
+    render() {
+        let { exerciseArr, index } = this.state;
+        let name = exerciseArr[index].name;
+        let visual = exerciseArr[index].visual;
+        let time = exerciseArr[index].time;
+console.log(index)
         return (
             <div>
-                <Timer time={legworkout[0].time} />
-                {exercises}
+                {/* <Timer time={exerciseArr[index].time} /> */}
+                <div>
+                {/* <Timer time={exerciseArr[index].time} /> */}
+                    <div key={index}>
+                        <h1>exercise name : {name}</h1>
+                        <div> exercise visual : {visual}</div>
+                        <h2>exercise time : {time}</h2>
+                        <Timer time={time} />
+                    </div>
+                </div>
             </div>
-
         )
-
+    }
+    componentWillUnmount() {
+        clearInterval(this.updateInterval);
     }
 }
 
