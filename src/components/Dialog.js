821ef3b9c369e3a24e5d5718faedcad3cc78ec3e
shani <shani.mek@gmail.com>
2021-04-04 19:19:14 +0300

    s

--- src/components/Dialog.js
+++ src/components/Dialog.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './Dialog.css';
 import { Link } from 'react-router-dom';
-// import Workout from '../pages/Workout';
 
 class Dialog extends React.Component {
     render() {
